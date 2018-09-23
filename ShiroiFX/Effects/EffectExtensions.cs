5a136fc9289069647551adf9d485d4b5faa0e442
Bruno Silva Freire <brunorbsf@gmail.com>
2018-09-23 13:08:47 -0300

    - Adding more Extension methods

--- ShiroiFX/Effects/EffectExtensions.cs
+++ ShiroiFX/Effects/EffectExtensions.cs
@@ -1,10 +1,36 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Shiroi.FX.Features;
-using UnityUtilities;
+using UnityEngine;
 
 namespace Shiroi.FX.Effects {
     public static class EffectExtensions {
-        public static void ThrowIncompatibleFeaturesException(
+        public delegate EffectContext ContextCreator();
+
+        public static void PlayIfPresent(this Effect effect, EffectContext context) {
+            if (effect != null) {
+                effect.Play(context);
+            }
+        }
+
+        public static void PlayIfPresent(this Effect effect, ContextCreator contextCreator) {
+            if (effect != null) {
+                effect.Play(contextCreator());
+            }
+        }
+
+        public static void PlayIfPresent(this Effect effect, MonoBehaviour behaviour,
+            bool includePositionFeature = true, params EffectFeature[] features) {
+            var feature = new List<EffectFeature>();
+            feature.AddRange(features);
+            if (includePositionFeature) {
+                feature.Add(new PositionFeature(behaviour.transform.position));
+            }
+
+            effect.PlayIfPresent(new EffectContext(behaviour, feature.ToArray()));
+        }
+
+        private static void ThrowIncompatibleFeaturesException(
             this Effect effect,
             params EffectFeature[] incompatibleFeatures) {
             throw new IncompatibleFeaturesException(effect, incompatibleFeatures);
